e616ef1384399ca856037e8906509dd4c5352b38
pedro <pedro.lazari@adias.com.br>
2019-02-21 20:03:10 -0300

    Corrigindo erro de Escrita

--- database/migrations/2019_02_21_135128_create_products_table.php
+++ database/migrations/2019_02_21_135128_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePoductsTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreatePoductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('poducts', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->text('description');
@@ -39,6 +39,6 @@ class CreatePoductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('poducts');
+        Schema::dropIfExists('products');
     }
 }
