d385d357950412e1782bcd473ae134ef812b5cf4
pedro <pedro.lazari@adias.com.br>
2019-02-21 20:33:49 -0300

    Seeder de Categoria

--- database/seeds/CategoriesSeeder.php
+++ database/seeds/CategoriesSeeder.php
@@ -11,11 +11,88 @@ class CategoriesSeeder extends Seeder
      */
     public function run()
     {
-        \App\Categories::truncate();
+//        \App\Categories::truncate();
 
         \App\Categories::create([
             'name'          => 'Roupas',
-            'description'   => 'Categoria de Ropas'
+            'description'   => 'Categoria de Ropas',
+            'children' => [
+                [
+                    'name' => 'Masculino',
+                    'children' => [
+                        [
+                            'name' => 'Camisetas',
+                        ],
+                        [
+                            'name' => 'Calças',
+                        ],
+                        [
+                            'name' => 'Bermudas',
+                        ],
+                        [
+                            'name' => 'Sapatos',
+                        ],
+                        [
+                            'name' => 'Bonés',
+                        ],
+                    ]
+                ],
+                [
+                    'name' => 'Femininas',
+                    'children' => [
+                        [
+                            'name' => 'Camisetas',
+                        ],
+                        [
+                            'name' => 'Calças',
+                        ],
+                        [
+                            'name' => 'Saias',
+                        ],
+                        [
+                            'name' => 'Sapatos',
+                        ],
+                        [
+                            'name' => 'Bonés',
+                        ],
+                    ]
+                ]
+            ]
+        ]);
+
+        \App\Categories::create([
+            'name'          => 'Calçados',
+            'description'   => 'Categoria de Calçados',
+            'children' => [
+                [
+                    'name' => 'Masculino',
+                    'children' => [
+                        [
+                            'name' => 'Tamanho 36 ao 38',
+                        ],
+                        [
+                            'name' => 'Tamanho 39 ao 41',
+                        ],
+                        [
+                            'name' => 'Tamanho 41 ao 43',
+                        ],
+                    ]
+                ],
+                [
+                    'name' => 'Femininas',
+                    'children' => [
+                        [
+                            'name' => 'Tamanho 32 ao 34',
+                        ],
+                        [
+                            'name' => 'Tamanho 35 ao 37',
+                        ],
+                        [
+                            'name' => 'Tamanho 38 ao 40',
+                        ],
+                    ]
+                ]
+            ]
         ]);
     }
 }
