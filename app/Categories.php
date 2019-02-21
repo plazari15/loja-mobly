eb582a2fd9f6fdb560be1fdc995346be0dbfd5a9
pedro <pedro.lazari@adias.com.br>
2019-02-21 20:34:37 -0300

    INserindo classe de Node

--- app/Categories.php
+++ app/Categories.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Kalnoy\Nestedset\NodeTrait;
 
 class Categories extends Model
 {
+    use NodeTrait;
     protected $fillable = [
         'description',
         'name'
