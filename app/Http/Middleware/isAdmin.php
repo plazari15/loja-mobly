8fad9c205ed4bddd0ce9b5bf8a24d3f01069ff9e
pedro <pedro.lazari@adias.com.br>
2019-02-21 21:15:18 -0300

    Criando Middlewae

---
+++ app/Http/Middleware/isAdmin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Redirect;
+
+class isAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(!$request->user()->is_admin){
+            return Redirect::to('dashboard');
+        }
+
+        return $next($request);
+    }
+}
